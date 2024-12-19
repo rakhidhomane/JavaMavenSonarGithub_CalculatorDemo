bb7a2d139b1ce84acdee1e853770037f43a4da2d
Rakhi <rakhid@cybage.com>
2024-12-19 15:30:09 +0530

    first commit

---
+++ src/main/java/com/cybage/alm/App.java
@@ -0,0 +1,25 @@
+package com.cybage.alm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a - b; 
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+}
